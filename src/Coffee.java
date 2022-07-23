0d739a197944e365480faa4f7be74a6df7b44e84
tejaskhartude <tejaskhartude@gmail.com>
2022-07-23 18:52:09 +0530

    Added coffe

---
+++ src/Coffee.java
@@ -0,0 +1,8 @@
+abstract class Coffee {
+
+    public int getNumber(int x){
+        x=15;
+        return x;
+    }
+
+}
---
+++ src/Lion.java
@@ -0,0 +1,9 @@
+public class Lion extends Coffee{
+
+    @Override
+    public int getNumber(int x) {
+
+       x=15;
+       return x;
+    }
+}
